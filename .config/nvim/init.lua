31435dbb9d730e016d7a0ec5da11aed7c05af1aa
Jan Sembiring <jan.s@tetradyne.co.kr>
2025-08-07 00:07:40 +0900

    Add nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,4 @@
+vim.opt.clipboard = "unnamedplus"
+vim.opt.nu = true -- Enable absolute line numbers
+vim.opt.relativenumber = true -- Enable relative line numbers
+vim.opt.statuscolumn = "%s %l %r" -- Customize statuscolumn to display both
