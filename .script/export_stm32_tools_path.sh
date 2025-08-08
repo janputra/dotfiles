49965bb4179758a8f2214b87ada7a441d235169d
Jan Sembiring <jan.s@tetradyne.co.kr>
2025-08-08 18:19:41 +0900

    add script to load stm32 envi. variable

---
+++ .script/export_stm32_tools_path.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Adjust these base paths if your installation is in a different location
+BASE_DIR="/home/jan/st/stm32cubeclt_1.17.0"
+
+export PATH="$BASE_DIR/CMake/bin:$BASE_DIR/Ninja/bin:$BASE_DIR/GNU-tools-for-STM32/bin:$BASE_DIR/STM32CubeProgrammer/bin:$PATH"
+
