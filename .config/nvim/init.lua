4007e34a8510ef94e7adf0541a32a35f52de90f4
Jan Sembiring <jan.s@tetradyne.co.kr>
2025-08-18 09:41:50 +0900

    change app launcher icon to def, nvim search case sensitivity

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,9 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.nu = true -- Enable absolute line numbers
 vim.opt.relativenumber = true -- Enable relative line numbers
 vim.opt.statuscolumn = "%s %l %r" -- Customize statuscolumn to display both
+
+-- Set ignorecase to true for case-insensitive searching by default
+vim.o.ignorecase = true
+
+-- Set smartcase to true to override ignorecase if the search pattern contains uppercase characters
+vim.o.smartcase = true
