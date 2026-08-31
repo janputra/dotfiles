963e99509d7f8e27e29d2578ffb11dae4a8a27cf
Jan Sembiring <janpoetra22@gmail.com>
2026-08-31 19:59:13 +0900

    switch theme to catpuccin variant(except cosmic de)

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,6 +3,17 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font 'Maple Mono NF'
 config.font_size = 11.0
-config.color_scheme ='Dracula (Official)'
 
+-- Define your choice of themes for dark and light modes
+local function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Catppuccin Macchiato' -- Change to your favorite dark theme
+  else
+    return 'Catppuccin Latte'    -- Change to your favorite light theme
+  end
+end
+
+-- Set the initial color scheme based on current system appearance
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.use_fancy_tab_bar = false
 return config
